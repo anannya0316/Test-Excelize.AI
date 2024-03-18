c2c913e76e2d35569244d3b806d1140002a00c31
Anannya Chaudhary <92998423+anannya0316@users.noreply.github.com>
2024-03-18 18:06:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,27 +54,6 @@
     
 
 
-# import streamlit as st
-# from yolov9_code import perform_object_detection 
-# def main():
-#     st.title("Image Uploader")
-
-#     # File uploader widget
-#     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
-
-#     if uploaded_file is not None:
-#         # Display the uploaded image
-#         st.image(uploaded_file, caption='Uploaded Image', use_column_width=True)
-
-#         # You can also save the uploaded image if needed
-#         # with open('uploaded_image.jpg', 'wb') as f:
-#         #     f.write(uploaded_file.getbuffer())
-#         perform_object_detection(uploaded_file)
-
-# if __name__ == "__main__":
-#     main()
-
-
 import streamlit as st
 from yolov9_code import perform_object_detection
 
