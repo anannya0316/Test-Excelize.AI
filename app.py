7e9d911cfb3191737daf4f61d3d599270a10404e
Anannya Chaudhary <92998423+anannya0316@users.noreply.github.com>
2024-03-18 18:00:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,6 +56,26 @@
 
 import streamlit as st
 
+def perform_object_detection(uploaded_file):
+  from google.colab import drive
+  drive.mount('/content/drive')
+  %cd /content/drive/MyDrive/yolov9-main
+  !python detect.py --weights /content/drive/MyDrive/yolov9-main/yolov9-c.pt --source {uploaded_file} --device cpu
+  import os
+  from IPython.display import Image, display
+
+  output_dir = "/content/drive/MyDrive/yolov9-main/runs/train/exp9"
+
+  # Get a list of all files in the directory
+  file_list = os.listdir(output_dir)
+
+  # Filter out only the image files
+  image_files = [f for f in file_list if f.endswith('.jpg')]
+
+  # Display each image
+  for image_file in image_files:
+      display(Image(filename=os.path.join(output_dir, image_file), width=1000))
+          
 def main():
     st.title("Image Uploader")
 
@@ -69,6 +89,7 @@ def main():
         # You can also save the uploaded image if needed
         # with open('uploaded_image.jpg', 'wb') as f:
         #     f.write(uploaded_file.getbuffer())
+        perform_object_detection(uploaded_file)
 
 if __name__ == "__main__":
     main()
