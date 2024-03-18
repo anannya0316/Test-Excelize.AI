4d7c272355a36ed8b9e88ef107ee399d5055929a
Anannya Chaudhary <92998423+anannya0316@users.noreply.github.com>
2024-03-18 18:01:52 +0530

    Update yolov9_code.py

--- yolov9_code.py
+++ yolov9_code.py
@@ -1,4 +1,23 @@
 
+def perform_object_detection(uploaded_file):
+  from google.colab import drive
+  drive.mount('/content/drive')
+  #%cd /content/drive/MyDrive/yolov9-main
+  !python detect.py --weights /content/drive/MyDrive/yolov9-main/yolov9-c.pt --source {uploaded_file} --device cpu
+  import os
+  from IPython.display import Image, display
+
+  output_dir = "/content/drive/MyDrive/yolov9-main/runs/train/exp9"
+
+  # Get a list of all files in the directory
+  file_list = os.listdir(output_dir)
+
+  # Filter out only the image files
+  image_files = [f for f in file_list if f.endswith('.jpg')]
+
+  # Display each image
+  for image_file in image_files:
+      display(Image(filename=os.path.join(output_dir, image_file), width=1000))
 
 # import streamlit as st
 # import os
