c672c2f623aba8be1169b60db15851c78cc130fa
Anannya Chaudhary <92998423+anannya0316@users.noreply.github.com>
2024-03-18 18:32:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
+
+import streamlit as st
+from yolov9_code import perform_object_detection
+
+def main():
+    st.title("Image Uploader")
+
+    # File uploader widget
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+
+    if uploaded_file is not None:
+        # Display the uploaded image
+        st.image(uploaded_file, caption='Uploaded Image', use_column_width=True)
+
+        # Call the perform_object_detection function
+        perform_object_detection(uploaded_file)
+
+if __name__ == "__main__":
+    main()
+
 # import streamlit as st
 # import os
 # import shutil
@@ -52,23 +72,3 @@
 # if image_path:
 #     perform_object_detection()
     
-
-
-import streamlit as st
-from yolov9_code import perform_object_detection
-
-def main():
-    st.title("Image Uploader")
-
-    # File uploader widget
-    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
-
-    if uploaded_file is not None:
-        # Display the uploaded image
-        st.image(uploaded_file, caption='Uploaded Image', use_column_width=True)
-
-        # Call the perform_object_detection function
-        perform_object_detection(uploaded_file)
-
-if __name__ == "__main__":
-    main()
