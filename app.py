a5368cffe496ccc96f42771bb61f874fe12d5b50
Anannya Chaudhary <92998423+anannya0316@users.noreply.github.com>
2024-03-19 09:25:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,26 +18,28 @@
 # if __name__ == "__main__":
 #     main()
 
-import streamlit as st
-from PIL import Image
+import subprocess
 import os
 from google.colab import drive
+from PIL import Image
 
-# Function to perform object detection
 def perform_object_detection(uploaded_file):
     # Mount Google Drive
     drive.mount('/content/drive')
 
-    # Run YOLOv9 detection script
-    !python detect.py --weights /content/drive/MyDrive/yolov9-main/yolov9-c.pt --source {uploaded_file} --device cpu
-    
+    # Define the path to the YOLOv9 detection script
+    detect_script = '/content/drive/MyDrive/yolov9-main/detect.py'
+
+    # Run YOLOv9 detection script using subprocess
+    subprocess.run(['python', detect_script, '--weights', '/content/drive/MyDrive/yolov9-main/yolov9-c.pt', '--source', str(uploaded_file), '--device', 'cpu'])
+
     # Define the output directory
     output_dir = "/content/drive/MyDrive/yolov9-main/runs/train/exp9"
 
     # Display the output images
     for filename in os.listdir(output_dir):
         if filename.endswith('.jpg'):
-            st.image(Image.open(os.path.join(output_dir, filename)), caption='Detected Objects', use_column_width=True)
+            display(Image.open(os.path.join(output_dir, filename)), caption='Detected Objects', use_column_width=True)
 
 def main():
     st.title("YOLOv9 Object Detection")
