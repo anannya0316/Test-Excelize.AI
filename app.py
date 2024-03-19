f19c7e7e25885f752e66ac8e2b486aad72f0008d
Anannya Chaudhary <92998423+anannya0316@users.noreply.github.com>
2024-03-19 09:38:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,26 +18,27 @@
 # if __name__ == "__main__":
 #     main()
 
-import streamlit as st
 import subprocess
 import os
 from PIL import Image
+import streamlit as st
 
 def perform_object_detection(uploaded_file):
     # Define the path to the YOLOv9 detection script
     detect_script = '/content/drive/MyDrive/yolov9-main/detect.py'
-
+    
+    # Define the directory to save the output images
+    output_dir = '/content/output_images'
+    os.makedirs(output_dir, exist_ok=True)  # Create the directory if it doesn't exist
+    
     # Run YOLOv9 detection script using subprocess
-    subprocess.run(['python', detect_script, '--weights', '/content/drive/MyDrive/yolov9-main/yolov9-c.pt', '--source', str(uploaded_file), '--device', 'cpu'])
-
-    # Define the output directory
-    output_dir = "/content/drive/MyDrive/yolov9-main/runs/train/exp9"
-
+    subprocess.run(['python', detect_script, '--weights', '/content/drive/MyDrive/yolov9-main/yolov9-c.pt', '--source', str(uploaded_file), '--device', 'cpu', '--output', output_dir])
+    
     # Display the output images
     for filename in os.listdir(output_dir):
         if filename.endswith('.jpg'):
-            st.image(Image.open(os.path.join(output_dir, filename)), caption='Detected Objects', use_column_width=True)
-
+            image_path = os.path.join(output_dir, filename)
+            st.image(image_path, caption='Detected Objects', use_column_width=True)
 
 def main():
     st.title("YOLOv9 Object Detection")
